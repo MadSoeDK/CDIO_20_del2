10c5f65e97439177d43d3ecabbd84310d5927fd6
Mads <kontakt@mswebpartner.dk>
2021-10-08 17:24:14 +0200

    Added constructer method that sets up objects with Field name and Field rent

--- src/Field.java
+++ src/Field.java
@@ -2,9 +2,15 @@ public class Field {
 
     //Initialize variables
     private int number;
-    private int price;
+    private int rent;
     String name;
     String description;
 
-
+    //Sets up field name and field rent for each object by using the arrays
+    public Field (String[] fieldsname, int[] fieldsrent) {
+        for (int i = 0; i > fieldsname.length && i > fieldsrent.length; i++) {
+            name = fieldsname[i];
+            rent = fieldsrent[i];
+        }
+    }
 }
