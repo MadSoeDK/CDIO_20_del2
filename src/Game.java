06e4e951e5048230ccdd14b91f4dbefd50a2b3a4
Mads <kontakt@mswebpartner.dk>
2021-10-08 17:25:07 +0200

    Added arrays with Field names & Field rent

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,14 @@
 public class Game {
     public static void main(String[] args) {
 
+        //Field name array
+        String[] fieldsname = { "Tower", "Crater", "Palace gates", "Cold Desert", "Walled city", "Monastery ", "Black cave", "Huts in the mountain", "The Werewall (werewolf-wall)", "The pit", "Goldmine" };
+
+        //Field rent array
+        int[] fieldsrent = { 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650 };
+
+        //Initilize all the fields with the arrays
+        Field Fields = new Field(fieldsname, fieldsrent);
+
     }
 }
