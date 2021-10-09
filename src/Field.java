760adf9797943731ba8327486467c0cfda570d22
Mads <kontakt@mswebpartner.dk>
2021-10-09 12:10:45 +0200

    Made the constructor simpler. Arrays are no longer parameters in the contructor.

--- src/Field.java
+++ src/Field.java
@@ -7,10 +7,8 @@ public class Field {
     String description;
 
     //Sets up field name and field rent for each object by using the arrays
-    public Field (String[] fieldsname, int[] fieldsrent) {
-        for (int i = 0; i > fieldsname.length && i > fieldsrent.length; i++) {
-            name = fieldsname[i];
-            rent = fieldsrent[i];
-        }
+    public Field (String fieldname, int fieldrent) {
+        fieldname = name;
+        fieldrent = rent;
     }
 }
