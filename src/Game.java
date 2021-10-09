c6231068c153fa9fdb4fde55d6ea74df51ae6865
Mads <kontakt@mswebpartner.dk>
2021-10-09 12:09:16 +0200

    Fields array now stores Field objects

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,19 @@
 public class Game {
     public static void main(String[] args) {
 
-        //Field name array
-        String[] fieldsname = { "Tower", "Crater", "Palace gates", "Cold Desert", "Walled city", "Monastery ", "Black cave", "Huts in the mountain", "The Werewall (werewolf-wall)", "The pit", "Goldmine" };
-
-        //Field rent array
-        int[] fieldsrent = { 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650 };
-
-        //Initilize all the fields with the arrays
-        Field Fields = new Field(fieldsname, fieldsrent);
-
+        //Instantiate each Field object and store in array
+        Field[] fields = {
+                new Field("Tower", 250),
+                new Field("Crater", -100),
+                new Field("Palace gates", 100),
+                new Field("Cold Desert", -20),
+                new Field("Walled city", 180),
+                new Field("Monastery", 0),
+                new Field("Black cave", -70),
+                new Field("Huts in the mountain", 60),
+                new Field("The Werewall (werewolf-wall)", -80),
+                new Field("The pit", -50),
+                new Field("Goldmine", 650),
+        };
     }
 }
