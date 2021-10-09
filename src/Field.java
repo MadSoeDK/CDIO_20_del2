ebd7387ef86a65337621be1353908ebbf9143d3c
Mads <kontakt@mswebpartner.dk>
2021-10-09 12:16:53 +0200

    Set up getters in Field class

--- src/Field.java
+++ src/Field.java
@@ -1,14 +1,23 @@
 public class Field {
 
     //Initialize variables
-    private int number;
+    //private int number;
     private int rent;
-    String name;
-    String description;
+    private String name;
+    private String description;
 
     //Sets up field name and field rent for each object by using the arrays
     public Field (String fieldname, int fieldrent) {
         fieldname = name;
         fieldrent = rent;
+    }
+    public int getRent() {
+        return rent;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
     }
 }
