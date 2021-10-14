f90ef49c01f211bdeed1e4e06bda15411167cae7
Mads <kontakt@mswebpartner.dk>
2021-10-14 13:01:09 +0200

    Removed old methods

--- src/View/TUI.java
+++ src/View/TUI.java
@@ -17,9 +17,7 @@ public class TUI {
                 "For at vælge dansk så klik 1 og Enter. To choose english press 2 and Enter.");
     }
 
-    //--------------------------------
     // User language selection
-    //--------------------------------
     public static int language(){
         int chosenLanguage;
 
@@ -37,9 +35,7 @@ public class TUI {
         return (chosenLanguage);
     }
 
-    //-------------------------
     // User selects playername and Player objects is created
-    //-------------------------
     public static void chooseNames() {
         System.out.println("How many do you want to play? You can play up to 4.");
         Scanner input = new Scanner(System.in);
@@ -62,20 +58,16 @@ public class TUI {
         input.nextLine();
     }
 
-    //-----------------------------
-    // Fortæller hvad spiller slog
-    //-----------------------------
     public static void printFaceValue(int rollSum){
         System.out.println(languageArray[6] + " " + rollSum);
     }
 
-    //--------------------------------
-    // Fortæller spillerens nye score
-    //--------------------------------
-    public static void printPlayerScore(Player player){
+    // Print player balance
+    public static void printPlayerBalance(Player player){
         System.out.println(languageArray[7] + " " + player.getPlayerBalance());
     }
 
+    //Print current
     public static void printCurrentField(Player player){
         System.out.println("Current Field is" + " " + GameController.getField(player.getPlacement()).getName());
     }
@@ -87,85 +79,19 @@ public class TUI {
         }
     }
 
-    /*
-    //----------------------------------------------
+    /* Unused stuff...
     // Fortæller spiller hvilket sprog de har valgt
-    //----------------------------------------------
     public static void printLanguage(){
         System.out.println(languageArray[0]);
     }
 
-    //---------------------------------------
-    // Får værdier til skabelse af terninger
-    //---------------------------------------
-    public static void printChooseDice(){
-        boolean areNumbers;
-
-        do {
-            try {
-                Scanner input = new Scanner(System.in);
-                System.out.println(languageArray[1]);
-                int die1 = input.nextInt();
-                int die2 = input.nextInt();
-                System.out.println(languageArray[2] + " " + die1 + " & " + die2);
-                dieArray = new int[]{die1, die2};
-                areNumbers = true;
-            }
-            catch (InputMismatchException e){
-                areNumbers = false;
-            }
-        } while (!areNumbers);
-    }
-
-        //-----------------------------
     // Fortæller hvad spiller slog
-    //-----------------------------
     public static void printFaceValue(){
         System.out.println(languageArray[6] + " " + Game.getDieFaceValue(0) + " + " + Game.getDieFaceValue(1) + " = " + Game.getRollSum());
     }
 
-    //--------------------------------
     // Fortæller spillerens nye score
-    //--------------------------------
     public static void printPlayerScore(int player){
         System.out.println(languageArray[7] + " " + Game.getPlayerScore(player));
-    }
-
-    //-------------------------------------------
-    // Fortæller spilleren at han/hun har vundet
-    //-------------------------------------------
-    public static void printWin1(int player){
-        System.out.println(Game.getPlayerName(player) + languageArray[8]);
-    }
-
-    //---------------------------------------------
-    // Fortæller spillere hvis de begge har vundet
-    //---------------------------------------------
-    public static void printWin2(){
-        System.out.println(languageArray[9]);
-    }
-
-    //---------------------------------------------------------------
-    // Bruger flavor-text til at forklare spillerens ændring i score
-    //---------------------------------------------------------------
-    public static void printEndTurn(int input){
-        int line = input + 9;
-        if ((line >= 11) && (line <= 21)) {
-            System.out.println(languageArray[line]);
-        }
-        else{
-            System.out.println("FEJL");
-        }
-    }
-
-    //----------
-    // Gettters
-    //----------
-    public static String getName(int nameIndex) {
-        return nameArray[nameIndex];
-    }
-
-    public static int getDieFace(int dieIndex){
-        return dieArray[dieIndex];
     }*/
 }
