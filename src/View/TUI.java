309e14f4200a525915f68bad8c7a833ce92b1d7d
Mads <kontakt@mswebpartner.dk>
2021-10-15 10:26:20 +0200

    Added Startgame method

--- src/View/TUI.java
+++ src/View/TUI.java
@@ -3,4 +3,10 @@ import java.util.Scanner;
 
 public class TUI {
 
+    public static void startGame() {
+        System.out.println("Velkommen til spillet.\n Vælg venligst et sprog (Please choose a language).\n" +
+                "For at vælge dansk så klik 1 og Enter. To choose english press 2 and Enter.");
+    }
+
+
 }
