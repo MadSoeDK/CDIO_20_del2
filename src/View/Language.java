9e246797960810afcf576bf58766819e37e6da5b
Mads <kontakt@mswebpartner.dk>
2021-10-24 21:17:00 +0200

    language array fixed length

--- src/View/Language.java
+++ src/View/Language.java
@@ -9,7 +9,7 @@ public class Language {
 
     // Læser den rigtige sprog fil, og deler den op i et array
     public static String[] chooseLanguage(int a) {
-        languageArray = new String[6];
+        languageArray = new String[7];
 
         if (a == 1) {
             try {
