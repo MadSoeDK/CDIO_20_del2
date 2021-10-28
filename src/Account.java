a42b8d4bab51666e06ebf78faa1990805fff4201
Mads <kontakt@mswebpartner.dk>
2021-10-28 12:13:14 +0200

    Added check for negative balance in setBalance

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.abs;
+
 public class Account {
 
     private int balance;
@@ -7,8 +9,16 @@ public class Account {
         balance = 1000;
     }
 
+    /**
+     * Changes the balance. The balance can't be negative, and is then set to 0.
+     * @param amount Positive or negative integer
+     */
     public void setBalance(int amount) {
-        balance = balance + amount;
+        if(amount < 0 && amount > balance) {
+            balance = 0;
+        } else {
+            balance = balance + amount;
+        }
     }
 
     public int getBalance() {
